853ed808cea9061a3ffc4d805d6b6b017fb67981
daswe_23 <veselkovapolina9@gmail.com>
2023-09-29 13:16:03 +0400

    Отредактированы отступы, табуляция [2]

--- main.py
+++ main.py
@@ -17,16 +17,16 @@ while counter < 100:
     print('attempt =', counter)
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
-if updates['result']:
-    for result in updates['result']:
-        offset = result['update_id']
-        chat_id = result['message']['from']['id']
-        cat_response = requests.get(API_CATS_URL)
-        if cat_response.status_code == 200:
-            cat_link = cat_response.json()[0]['url']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
-        else:
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
 
-time.sleep(1)
-counter += 1
+    time.sleep(1)
+    counter += 1
